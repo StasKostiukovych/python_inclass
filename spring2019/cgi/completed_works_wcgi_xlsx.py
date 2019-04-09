443df081c2e024ef63121e1a7331830f80d7828c
Stas <40966926+StasKostiukovych@users.noreply.github.com>
2019-04-09 18:36:50 +0300

    Update completed_works_wcgi_xlsx.py

--- spring2019/cgi/completed_works_wcgi_xlsx.py
+++ spring2019/cgi/completed_works_wcgi_xlsx.py
@@ -1,5 +1,6 @@
+from openpyxl import load_workbook
 import cgi
-
+from wsgiref.simple_server import make_server
 
 HTML_PAGE = """<html>
 <title>registration</title>
@@ -9,17 +10,22 @@ HTML_PAGE = """<html>
 
 <br>
 <br>
-<form method=POST action={}>
+<form method=POST action=>
 <table>
 <tr>
 <td align=right>
 
 Type of act:
-{}
+<select name="regim">
+<option value="Grounds">Grounds</option>
+<option value="Works">Works</option>
+<option value="Acts">Acts</option>
+<option value="Points">Points</option>
+</select>
 <br>
 
 
-</td>
+</td>   
 <tr>
 <td colspan=2 align=center>
 <input type=submit value="select">
@@ -31,166 +37,198 @@ Type of act:
 """
 
 
-GROUNDS_HTML = """
-ID:
-<input type=text name=n_val1 value="">
-<br>
-
-Name:
-<input type=text name=n_val2 value="">
-<br>
-
-Adress:
-<input type=text name=n_val3 value="">
-<br>
-
-Responsible:
-<input type=text name=n_val4 value="">
-<br>
-
-Manager:
-<input type=text name=n_val5 value="">
-<br>
-
+GROUNDS_HTML = """<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Стипендія</title>
+</head>
+<body>
+<form method=POST action="">
+<h1 style="text-align: center">Додати інформацію про студента</h1>
+<p style="text-align: center">
+<font size="4" color="blue">Заповніть поля:</font><br>
+<table align="center" style="text-align: center">
 <tr>
-<td colspan=2 align=center>
-<input type="submit" value="Submit" name="Submit1" />
-</td>
-"""
+<td>ID:</td>
+<td><input type=text name=s1 value=""></td>
+<td>Name:</td>
+<td><input type=text name=s2 value=""></td>
+<td>Address:</td>
+<td><input type=text name=s3 value=""></td>
+<td>Responsible:</td>
+<td><input type=text name=s4 value=""></td>
+<td>Manager:</td>
+<td><input type=text name=s5 value=""></td>
 
-WORKS_HTML = """
-ID:
-<input type=text name=n_val1 value="">
+</table>
+<input type=submit value="Add">
 <br>
+</form>
+</body>
+</html>"""
 
-Name:
-<input type=text name=n_val2 value="">
-<br>
 
+
+WORKS_HTML = """<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Стипендія</title>
+</head>
+<body>
+<form method=POST action="">
+<h1 style="text-align: center">Додати інформацію про студента</h1>
+<p style="text-align: center">
+<font size="4" color="blue">Заповніть поля:</font><br>
+<table align="center" style="text-align: center">
 <tr>
-<td colspan=2 align=center>
-<input type="submit" value="Submit" name="Submit2" />
-</td>
-"""
+<td>ID:</td>
+<td><input type=text name=s6 value=""></td>
+<td>Name:</td>
+<td><input type=text name=s7 value=""></td>
+<td>Address:</td>
 
-
-ACTS_HTML = """
-ID:
-<input type=text name=n_val1 value="">
+</table>
+<input type=submit value="Add">
 <br>
+</form>
+</body>
+</html>"""
 
-No:
-<input type=text name=n_val2 value="">
-<br>
-
-Date:
-<input type=text name=n_val3 value="">
-<br>
-
-Sum:
-<input type=text name=n_val4 value="">
-<br>
-
-S_id:
-<input type=text name=n_val5 value="">
-<br>
-
+ACTS_HTML = """<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Стипендія</title>
+</head>
+<body>
+<form method=POST action="">
+<h1 style="text-align: center">Додати інформацію про студента</h1>
+<p style="text-align: center">
+<font size="4" color="blue">Заповніть поля:</font><br>
+<table align="center" style="text-align: center">
 <tr>
-<td colspan=2 align=center>
-<input type="submit" value="Submit" name="Submit3" />
-</td>
-"""
+<td>ID:</td>
+<td><input type=text name=s8 value=""></td>
+<td>No:</td>
+<td><input type=text name=s9 value=""></td>
+<td>Date:</td>
+<td><input type=text name=s10 value=""></td>
+<td>Sum:</td>
+<td><input type=text name=s11 value=""></td>
+<td>S_id:</td>
+<td><input type=text name=s12 value=""></td>
 
-POINTS_HTML = """
-W_id:
-<input type=text name=n_val1 value="">
+</table>
+<input type=submit value="Add">
 <br>
+</form>
+</body>
+</html>"""
 
-A_id:
-<input type=text name=n_val2 value="">
-<br>
-
+POINTS_HTML = """<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Стипендія</title>
+</head>
+<body>
+<form method=POST action="">
+<h1 style="text-align: center">Додати інформацію про студента</h1>
+<p style="text-align: center">
+<font size="4" color="blue">Заповніть поля:</font><br>
+<table align="center" style="text-align: center">
 <tr>
-<td colspan=2 align=center>
-<input type="submit" value="Submit" name="Submit4" />
-</td>
-"""
+<td>W_id:</td>
+<td><input type=text name=s13 value=""></td>
+<td>A_id:</td>
+<td><input type=text name=s14 value=""></td>
+</table>
+<input type=submit value="Add">
+<br>
+</form>
+</body>
+</html>"""
 
 
-class Works:
+def to_xml(array, sheet, filename="info.xlsx"):
+    wb = load_workbook(filename)
+    ws = ""
+    try:
+        ws = wb[sheet]
+    except:
+        wb.create_sheet(sheet)
+        ws = wb[sheet]
 
-    def __init__(self, ground_xlsx="ground.xlsx", works_xlsx="works.xlsx",
-                 acts_xlsx="acts.xlsx", points_xlsx="points.xlsx"):
-
-        self.ground_xlsx = ground_xlsx
-        self.works_xlsx = works_xlsx
-        self.acts_xlsx = acts_xlsx
-        self.points_xlsx =points_xlsx
-        self.info_list = ["Майданчики", "Роботи", "Акти", "Пункти"]
-
-
-    def mkslct(self, name, values):
-
-        select_block = '<select name="{0}">\n{1}</select>\n'
-
-        option_block = '<option value="{0}">{0}</option>\n'
-
-        return select_block.format(name, ''.join(option_block.format(v) for v in values))
+    ws.append(array)
+    wb.save(filename=filename)
 
 
-    def one_of_four(self, option):
-        if option == "Майданчики":
-            return GROUNDS_HTML
-
-        elif option == "Роботи":
-            return WORKS_HTML
-
-        elif option == "Акти":
-            return ACTS_HTML
-
-        elif option == "Пункти":
-            return POINTS_HTML
-
-    def web_page(self, environ, start_response):
-
-        if environ.get('PATH_INFO', '').lstrip('/') == '':
-
-            form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+state = 0
 
 
-            result = form.getvalue("list")
+def application(environ, start_response):
 
-            if "Submit1" in form:
-                templ = 1
+    global state
 
-            elif "Submit2" in form:
-                templ = 2
+    if environ.get('PATH_INFO', '').lstrip('/') == '':
+        form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
-            elif "Submit1" in form:
-                templ = 3
+        html = HTML_PAGE
 
-            elif "Submit4" in form:
-                templ = 4
-            else:
-                templ = "Couldn't determine which button was pressed."
+        if state == 1:
+            if 's1' in form and 's2' in form and 's3' in form and 's4' in form:
+                to_xml([str(form['s1'].value), str(form['s2'].value), str(form['s3'].value),
+                        str(form['s4'].value)], "grounds")
 
-            body = HTML_PAGE.format(self.one_of_four(result) ,self.mkslct("list", self.info_list) )
+        elif state == 2:
+            if 's6' in form and 's7' in form:
+                to_xml([str(form['s1'].value), str(form['s2'].value)], "works")
 
-            start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+
+        elif state == 3:
+            if 's8' in form and 's9' in form and 's10' in form and 's11' in form and 's12' in form:
+                to_xml([str(form['s8'].value), str(form['s9'].value), str(form['s10'].value),
+                        str(form['s11'].value), str(form['s12'].value)], "acts")
+
+        elif state == 4:
+            if 's13' in form and 's14' in form:
+                to_xml([str(form['s8'].value), str(form['s9'].value)], "points")
+                
         else:
+            html = HTML_PAGE
 
-            start_response('404 NOT FOUND', [('Content-Type', 'text/plain; charset=utf-8')])
-            body = 'Сторінку не знайдено'
+        if 'regim' in form:
+            val = form['regim'].value
 
-        return [bytes(body, encoding='utf-8')]
+            if val == 'Grounds':
+                state = 1
+                html = GROUNDS_HTML
+
+            elif val == 'Works':
+                state = 2
+                html = WORKS_HTML
+
+            elif val == 'Acts':
+                state = 3
+                html = ACTS_HTML
+
+            elif val == 'Points':
+                state = 4
+                html = POINTS_HTML
+
+        else:
+            state = 0
+
+        body = html
+        start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+
+    else:
+
+        start_response('404 NOT FOUND', [('Content-Type', 'text/plain; charset=utf-8')])
+        body = 'Сторінку не знайдено'
+
+    return [bytes(body, encoding='utf-8')]
 
 
+print('=== Local WSGI webserver ===')
+httpd = make_server('localhost', 8090, application)
+httpd.serve_forever()
 
-    def connect(self):
-        from wsgiref.simple_server import make_server
-        print('=== Local WSGI webserver ===')
-        httpd = make_server('localhost', 8046, self.web_page)
-        httpd.serve_forever()
-
-w = Works()
-w.connect()
